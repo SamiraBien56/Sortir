e0b92373092ca5ae34929f4984136bf4af794002
L-Hes <heslot.lisa@outlook.fr>
2021-05-04 15:36:59 +0200

    filtre des sorties

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,8 +4,11 @@
 namespace App\Controller;
 
 
+use App\Form\FilterListType;
+use App\Repository\ParticipantRepository;
 use App\utils\SortieManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -16,15 +19,24 @@ class MainController extends AbstractController
      * @Route ("/", name="main_home")
      *
      */
-    public function home(SortieManager $sortieManager)
+    public function home(SortieManager $sortieManager, Request $request, ParticipantRepository $participantRepository)
     {
-        if($this->getUser() != null) {
-            $listAllSorties = $sortieManager->getAllSorties();
+        $filterForm = $this->createForm(FilterListType::class);
+        $filterForm->handleRequest($request);
+        if ($filterForm->isSubmitted() && $filterForm->isValid()) {
+            $req = $request->request->all("filter_list");
+            $listAllSorties = $sortieManager->getSortiesByFilter($req["campus"], $req["dateMin"], $req["dateMax"]);
         } else {
-            $listAllSorties = $sortieManager->getAllSorties();
+            if($this->getUser() != null) {
+                $user = $participantRepository->find($this->getUser()->getId());
+                $listAllSorties = $sortieManager->getSortiesByCampus($user->getCampus()->getId());
+            } else {
+                $listAllSorties = $sortieManager->getAllSorties();
+            }
         }
         return $this->render('main/home.html.twig', [
-            'listAllSorties' => $listAllSorties
+            'listAllSorties' => $listAllSorties,
+            'filterForm'=> $filterForm->createView()
         ]);
     }
 
