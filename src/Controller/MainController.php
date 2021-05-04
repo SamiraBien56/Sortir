9cae66592b8c8d2dc5ead50f487eb70af973ec11
samira BOULAROUAH <samira.boularouah2020@campus-eni.fr>
2021-05-04 16:35:10 +0200

    Liens dans home/actions

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 
+
+use App\Repository\SortieRepository;
 use App\utils\SortieManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,15 +18,23 @@ class MainController extends AbstractController
      * @Route ("/", name="main_home")
      *
      */
-    public function home(SortieManager $sortieManager)
+    public function home(SortieManager $sortieManager,SortieRepository $sortieRepository)
     {
+    $inscriptions=null;
+
+
         if($this->getUser() != null) {
+            $inscriptions = $this->getUser()->getInscriptions();
             $listAllSorties = $sortieManager->getAllSorties();
+
         } else {
             $listAllSorties = $sortieManager->getAllSorties();
         }
         return $this->render('main/home.html.twig', [
-            'listAllSorties' => $listAllSorties
+            'listAllSorties' => $listAllSorties,
+            'inscription'=>$inscriptions
+
+
         ]);
     }
 
