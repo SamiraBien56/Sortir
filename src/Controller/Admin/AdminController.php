6c2787821d8da35f506f253f6d997074db88fe28
L-Hes <heslot.lisa@outlook.fr>
2021-05-06 17:37:34 +0200

    Admin annuler sortie ok Attention créer sortie en chantier !

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\EtatRepository;
+use App\Repository\SortieRepository;
+use App\utils\SortieManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,5 +24,30 @@ class AdminController extends AbstractController
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
+    }
+
+    /**
+     * @Route("/sorties", name="sorties")
+     */
+    public function sorties(SortieManager $sortieManager): Response
+    {
+        $sorties = $sortieManager->getAllSorties();
+
+        return $this->render('admin/sorties.html.twig', [
+            'sorties' => $sorties
+        ]);
+    }
+
+    /**
+     * @Route("/sorties/annuler/{id}", name="sorties_annuler")
+     */
+    public function annuler($id, SortieRepository $sortieRepository, EtatRepository $etatRepository, EntityManagerInterface $entityManager): Response
+    {
+        $sortie = $sortieRepository->find($id);
+        $etat = $etatRepository->find(6);
+        $sortie->setEtat($etat);
+        $entityManager->flush();
+        $this->addFlash('success', 'La sortie a bien été annulée');
+        return $this->redirectToRoute('admin_sorties');
     }
 }
